d8537e60c1f885401fa724fa676b291fdba27547
Sewedo Todowede <todowede@gmail.com>
2021-09-13 00:18:14 +0100

    updated code et al

--- Image_Classification/datasets/simpledatasetloader.py
+++ Image_Classification/datasets/simpledatasetloader.py
@@ -37,7 +37,7 @@ class SimpleDatasetLoader:
             labels.append(label)
 
             # show an update every 'verbose' images
-            if verbose >> 0 and i > 0 and (i + 1) % verbose == 0:
+            if verbose > 0 and i > 0 and (i + 1) % verbose == 0:
                 print("[INFO] processed {}/{}". format(i + 1,
                       len(imagePaths)))
 
