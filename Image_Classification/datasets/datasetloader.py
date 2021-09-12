11c8f8a597d51f5456d1eec0b0b728a64dd34f1d
Sewedo Todowede <todowede@gmail.com>
2021-09-12 01:02:09 +0100

    wrote more codes

---
+++ Image_Classification/datasets/datasetloader.py
@@ -0,0 +1,15 @@
+# Import library
+import numpy as np
+import cv2
+import os
+
+
+class DatasetLoader:
+    def __init__(self, preprocessors=None):
+        # store the image preprocessor
+        self.preprocessors = preprocessors
+
+        # if the preprocessors are None, initialize them as an empty list
+        if self.preprocessors is None:
+            self.preprocessors = []
+            
---
+++ Image_Classification/preprocessing/preprocessor.py
@@ -0,0 +1,19 @@
+# import package
+import cv2
+
+# Create Preprocessor class for the preprocessing functions.
+class Preprocessor:
+    def __init__(self, width, height, inter=cv2.INTER_AREA):
+        # define the constructor to the Preprocessor class
+        # it requires two argument and optional third
+        # width: target width of input image after resizing
+        # height: target height if input image after resizing
+        # inter: optional - interpolation algorithm to use
+        self.width = width
+        self.height = height
+        self.inter = inter
+
+    def preprocess(self, image):
+        # The preprocess function; take a single argument - input image
+        return cv2.resize(image, (self.width, self.height),
+                          interpolation=self.inter)
